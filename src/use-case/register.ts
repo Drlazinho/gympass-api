cfc6d705af0acc64cb5d2deaa6776b790a286a79
Lázaro Pimentel <lazbonfim@hotmail.com>
2024-11-30 00:33:26 -0300

    Inversão de Dependencia (DIP)

--- src/use-case/register.ts
+++ src/use-case/register.ts
@@ -1,33 +1,32 @@
-import { PrismaUserRepository } from './../repositories/prisma-users-repository';
-import { prisma } from "@/lib/prisma"
-import { hash } from "bcryptjs"
+import { prisma } from '@/lib/prisma'
+import { hash } from 'bcryptjs'
 
 interface RegisterUseCaseRequest {
-    name: string;
-    email: string;
-    password: string;
+  name: string
+  email: string
+  password: string
 }
 
-export async function registerUseCase({
-name, email, password
-}: RegisterUseCaseRequest) {
-    const password_hash = await hash(password, 6) 
+export class RegisterUseCase {
+  constructor(private usersRepository: any) {}
+
+  async execute({ name, email, password }: RegisterUseCaseRequest) {
+    const password_hash = await hash(password, 6)
 
     const userWithSameEmail = await prisma.user.findUnique({
       where: {
         email,
       },
     })
-  
-    if (userWithSameEmail) {
-        throw new Error('Email already exists.')
-    }
-  
-    const prismaUserRepository = new PrismaUserRepository()
 
-    await prismaUserRepository.create({
-        name,
-        email,
-        password_hash,
+    if (userWithSameEmail) {
+      throw new Error('E-mail already exists.')
+    }
+
+    await this.usersRepository.create({
+      name,
+      email,
+      password_hash,
     })
+  }
 }
